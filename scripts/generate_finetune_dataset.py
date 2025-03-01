e862fdac9d0b76dd92c63cbd4f57d27d2a04552a
savnkk <39103342+natask@users.noreply.github.com>
2025-03-01 04:17:07 -0500

    (finetune): best practices for llama 70B

--- scripts/generate_finetune_dataset.py
+++ scripts/generate_finetune_dataset.py
@@ -23,7 +23,7 @@ def main():
         parser.add_argument("--model_name", type=str, default="casperhansen/llama-3.3-70b-instruct-awq", help="Hugging Face model name or path")
         parser.add_argument("--dataset_name", type=str, default='final_dataset.parquet', help="Name of the dataset file (without path) in parquet")
         parser.add_argument("--batch_size", type=int, default=8, help="Batch size for inference")
-        #parser.add_argument("--max_length", type=int, default=50, help="Max generation length")
+        parser.add_argument("--max_length", type=int, default=2080, help="Max generation length")
         args = parser.parse_args()
 
         # Prepare the results DataFrame
@@ -38,16 +38,29 @@ def main():
         # Prepare the results DataFrame
         results = []
 
+        # params for generate
+        num_beams = 4
+        no_repeat_ngram_size = 3
+        early_stopping = True
+
         # Load dataset
+        df = pd.read_parquet(os.path.join(base_dir, "datasets", args.dataset_name))
         for index, row in df.iterrows():
             question = row['question']
-            
+            question = f"""<|begin_of_text|>
+<|system|>
+You are a helpful assistant.
+<|end_of_system|>
+
+{question}
+
+<|end_of_text|>"""
             # Tokenize the input question
             inputs = tokenizer(question, return_tensors='pt').to(model.device)
             
             # Generate the solution
             with torch.no_grad():
-                outputs = model.generate(**inputs)  # Adjust max_length as needed , max_length=200
+                outputs = model.generate(**inputs, max_length=max_length, num_beams=num_beams, no_repeat_ngram_size=no_repeat_ngram_size, early_stopping=early_stopping)  # Adjust max_length as needed , max_length=200
             
             # Decode the generated solution
             solution = tokenizer.decode(outputs[0], skip_special_tokens=True)
